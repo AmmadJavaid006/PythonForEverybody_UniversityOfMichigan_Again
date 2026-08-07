72d004fc0c7b228c7ae05451dd924affef2b830a
AmmadJavaid006 <ammad.javaid420@gmail.com>
2026-08-08 00:03:36 +0500

    Merge

---
+++ Pay_Roll_Calc.py
@@ -0,0 +1,6 @@
+hrs = float(input("Enter Hours: ")) # Enter 35 Hours 
+rate = float(input("Enter Rate: ")) # Enter Rate of 2.75
+
+gpay = hrs * rate
+
+print("Gross Pay:", gpay) # Output: Gross Pay: 96.25
---
+++ Readme.txt
@@ -0,0 +1,4 @@
+PythonForEverybody by Dr Chuck (Second Time)
+
+1. Welcome.py
+2. Pay_Roll_Calc.py
