19f4d1fb897e47dc60c2b3c83e748d225ff2018d
AmmadJavaid006 <ammad.javaid420@gmail.com>
2026-08-12 21:09:10 +0500

    Readme.txt updates

--- Readme.txt
+++ Readme.txt
@@ -7,3 +7,16 @@ PythonForEverybody by Dr Chuck (Second Time)
 5. Functional_Pay_Roll_Calc.py
 6. Iteration_Loops.py
 7. String_Methods.py
+8. Read_File.py
+9. Read_File_Avg.py
+10. Dict_Count.py
+12. Word_Sort.py
+13. Concept_Tweaker.py
+14. Adress_Stripper.py
+
+
+TXT
+
+1. words.txt
+2. mbox-short.txt
+3. romeo.txt
--- address_stripper.py
+++ address_stripper.py
@@ -1,16 +1,16 @@
-    fname = input("Enter File Name: ")
-    count = 0
+fname = input("Enter File Name: ")
+count = 0
 
-    try:
-        fhandle = open(fname, "r")
-    except:
-        print("File Not Found:", fname)
-        quit()
+try:
+    fhandle = open(fname, "r")
+except:
+    print("File Not Found:", fname)
+    quit()
 
-    for line in fhandle:
-        if line.startswith("From "): # the space in "From " handles the exception of From: as "From " matches with "From " but not with "From:"
-            add = line.rstrip().split()
-            count += 1
-            print((add[1]))
+for line in fhandle:
+    if line.startswith("From "): # the space in "From " handles the exception of From: as "From " matches with "From " but not with "From:"
+        add = line.rstrip().split()
+        count += 1
+        print((add[1]))
 
-    print("There were", count, "lines in the file with From as the first word")
+print("There were", count, "lines in the file with From as the first word")
